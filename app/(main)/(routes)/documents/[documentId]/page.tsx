6785a19cdfadb7c4611ddcadcc6558172087b0a3
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2023-12-06 14:59:21 +0700

    feat: add toolbar

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+'use client'
 
-function DocumentIdPage() {
+import { Toolbar } from '@/components/toolbar'
+import { api } from '@/convex/_generated/api'
+import { Id } from '@/convex/_generated/dataModel'
+import { useQuery } from 'convex/react'
+import React from 'react'
+import { isNull } from 'util'
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<'documents'>
+  }
+
+}
+
+function DocumentIdPage({ params: { documentId } }: DocumentIdPageProps) {
+  const document = useQuery(api.documents.getById, {
+    documentId
+  })
+
+  if (document === undefined) {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
+
+  if (document === null) {
+    return <div>Not found</div>
+  }
+
   return (
-    <div>DocumentIdPage</div>
+    <div className='pb-40'>
+      <div className='h-52'></div>
+      <div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
+        <Toolbar
+          initialData={document}
+        />
+      </div>
+    </div>
   )
 }
 
