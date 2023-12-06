15d27b37479f473ec445e8f56066557a5dcd9103
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2023-12-06 17:34:21 +0700

    feat: add cover image

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 
+import { Cover } from '@/components/cover'
 import { Toolbar } from '@/components/toolbar'
+import { Skeleton } from '@/components/ui/skeleton'
 import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
 import { useQuery } from 'convex/react'
 import React from 'react'
-import { isNull } from 'util'
 
 interface DocumentIdPageProps {
   params: {
@@ -22,7 +23,15 @@ function DocumentIdPage({ params: { documentId } }: DocumentIdPageProps) {
   if (document === undefined) {
     return (
       <div>
-        Loading...
+        <Cover.Skeleton />
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-4 w-[80%]" />
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-4 w-[60%]" />
+          </div>
+        </div>
       </div>
     )
   }
@@ -33,7 +42,7 @@ function DocumentIdPage({ params: { documentId } }: DocumentIdPageProps) {
 
   return (
     <div className='pb-40'>
-      <div className='h-52'></div>
+      <Cover url={document.coverImage} />
       <div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
         <Toolbar
           initialData={document}
