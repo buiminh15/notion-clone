0a1ebb05ce811141c1dc1ba99fb2562232d02a60
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2023-12-07 11:29:53 +0700

    feat: publish doc

--- components/editor.tsx
+++ components/editor.tsx
@@ -4,10 +4,7 @@ import { useTheme } from "next-themes";
 import {
   BlockNoteEditor,
 } from "@blocknote/core";
-import {
-  BlockNoteView,
-  useBlockNote
-} from "@blocknote/react";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import "@blocknote/core/style.css";
 
 import { useEdgeStore } from "@/lib/edgestore";
